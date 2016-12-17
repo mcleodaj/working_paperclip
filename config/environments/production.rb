a2271cd8720ece659cc45326541c7faad5e7e307
Alex McLeod <mcleoda@yahoo.com>
2016-12-16 19:02:04 -0800

    set up paperclip to work with authenticated S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,12 @@ PaperclipDemo::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Amazon S3 settings for Paperclip uploads
+  
   config.paperclip_defaults = {
     storage: :s3,
     s3_protocol: 'http',
     s3_credentials: {
-      bucket: ENV.fetch("AWS_BUCKET"),
+      bucket: ENV.fetch("S3_BUCKET_NAME"),
       access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
       secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
       s3_region: ENV.fetch("AWS_REGION"),
